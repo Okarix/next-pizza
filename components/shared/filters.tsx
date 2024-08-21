934ef4063e2edc226fb554c69076370457730df4
Adiar <datkaevadiar@gmail.com>
2024-08-21 17:13:04 +0500

    feat: range cost slider in filtres

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
+import { RangeSlider } from '../ui/range-slider';
 
 interface Props {
 	className?: string;
@@ -23,6 +25,32 @@ export const Filters: React.FC<Props> = ({ className }) => {
 				<FilterCheckbox
 					text='Новинки'
 					value='2'
+				/>
+			</div>
+
+			<div className='mt-5 border-y border-y-neutral-200 py-6 pb-7'>
+				<p className='font-bold mb-3'>Цена от и до:</p>
+				<div className='flex gap-3 mb-5'>
+					<Input
+						type='number'
+						placeholder='0'
+						min={0}
+						maxLength={5000}
+						defaultValue={0}
+					/>
+					<Input
+						type='number'
+						min={100}
+						maxLength={3499}
+						placeholder='5000'
+					/>
+				</div>
+
+				<RangeSlider
+					min={0}
+					max={3499}
+					step={10}
+					value={[0, 3499]}
 				/>
 			</div>
 		</div>
